d593f4e9fca3bd58845f58c4824fb48af92aea6a
Errorcode101 <78766995+Errorcode101@users.noreply.github.com>
2024-07-08 20:25:30 -0500

    Revised files with better- less overwhelming documentation.

--- lib/main.dart
+++ lib/main.dart
@@ -51,25 +51,42 @@ class _MyHomePageState extends State<MyHomePage> {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     return Scaffold(
+      // the scaffold widget is the core of the entire app allowing ui elements like navigation menus
       appBar: AppBar(
+        // the bar displayed at the top of the page
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the theme value from MyHomePage and use it to set a color based on our theme.
         title: Text(widget.title),
       ),
       body: Center(
         // Center takes a single child and positions it in the middle of the parent.
-        child: Column(
-          // Column takes child widgets and arranges them vertically.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Padding(
+          // Padding give space around widgets.
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            // Column takes child widgets and arranges them vertically.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const MyCustomWidget(text: 'Here is a box within a column'),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // Alignment for the boxes in the row.
+                children: [
+                  Expanded(child: MyCustomWidget(text: "here's a row")),
+                  Expanded(child: MyCustomWidget(text: "And the right side!")),
+                  // Expanded allows the widget to take up the entire space given even if the size changes.
+                ],
+              ),
+              const SizedBox(height: 50), // An invisible box to add spacing.
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -77,6 +94,30 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MyCustomWidget extends StatelessWidget {
+  // This is a custom widget built for use in other parts of code.
+  const MyCustomWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.surfaceVariant,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        // Adjust the padding to control how close the card is from the text.
+        child: SizedBox(
+          child: Text(text),
+        ),
+      ),
     );
   }
 }
